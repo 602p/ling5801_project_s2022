663f5b88f7274bda276400164d6960d799787b2a
Louis Goessling <louis@goessling.com>
2022-04-27 15:58:47 -0500

    More configurability; fix bugs when doing more free-form permutation

--- metafoma.py
+++ metafoma.py
@@ -4,37 +4,44 @@ from foma import score_foma_code
 def do_compute(x):
 	return (score_foma_code('\n'.join(i.get_text() for i in x), queries, correct), x)
 
-def do_permute(args, parts, aux):
+def do_permute(args, parts, options):
 	while True:
 		act = random.choice(args)
-		if act == 'kleene':
-			while True:
-				item = random.randint(0, len(parts) - 1)
-				fragment = parts[item]
-				stripped = fragment.strip('*+')
-				if stripped.isalpha() or stripped == '?':
-					already = "" if stripped == fragment else fragment[-1]
-					new = ["", "*", "+"]
-					new.remove(already)
-					parts[item] = stripped + random.choice(new)
-					return
+		if act == 'kleene' and parts:
+			item = random.randint(0, len(parts) - 1)
+			fragment = parts[item]
+			stripped = fragment.strip('*+')
+			if stripped.isalpha() or stripped == '?':
+				already = "" if stripped == fragment else fragment[-1]
+				new = ["", "*", "+"]
+				new.remove(already)
+				parts[item] = stripped + random.choice(new)
+				return
 		elif act == 'presence':
-			if random.choice((0, 1)):
-				if len(parts) > 1:
-					parts.remove(random.choice(parts))
-					return
-			else:
-				if len(parts) < len(aux):
-					parts.insert(random.randint(0, len(parts)-1), random.choice(aux))
-					return
-		elif act == 'order':
+			if random.choice((0, 1)) and len(parts) > options['minlen']:
+				parts.remove(random.choice(parts))
+				return
+			elif len(parts) < options['maxlen']:
+				parts.insert(random.randint(0, len(parts)-1), random.choice(options['choices']))
+				return
+		elif act == 'order' and parts:
 			shuf_a = random.randint(0, len(parts) - 1)
 			shuf_b = random.randint(0, len(parts) - 1)
-			if shuf_a == shuf_b:
+			if shuf_a != shuf_b:
+				temp = parts[shuf_a]
+				parts[shuf_a] = parts[shuf_b]
+				parts[shuf_b] = temp
 				return
-			temp = parts[shuf_a]
-			parts[shuf_a] = parts[shuf_b]
-			parts[shuf_b] = temp
+
+def fixup_optargs(items, optargs):
+	v = optargs.get('choices', ' '.join(items))
+	optargs['choices'] = v.split(' ')
+
+	v = optargs.get('minlen', 1)
+	optargs['minlen'] = int(v)
+
+	v = optargs.get('maxlen', len(items))
+	optargs['maxlen'] = int(v)
 
 class LiteralChunk:
 	def __init__(self, lineno, lines):
@@ -52,25 +59,32 @@ class LiteralChunk:
 		return self
 
 class PermuteLinesChunk:
-	def __init__(self, lineno, args, lines):
+	def __init__(self, lineno, args, optargs, lines):
 		self.lineno = lineno
 		self.lines = lines
 		self.args = args
+		self.optargs = optargs
 
 	def permute(self):
-		do_permute(self.args, self.lines, self.lines)
+		do_permute(self.args, self.lines, self.optargs)
 
 	def get_text(self):
 		return '\n'.join(self.lines)
 
 	def copy(self):
-		return PermuteLinesChunk(self.lineno, self.args, self.lines[:])
+		return PermuteLinesChunk(self.lineno, self.args, self.optargs, self.lines[:])
+
+	def shortstr(self):
+		return f"lines({', '.join(self.args)})"
+
+	def dbgstate(self):
+		return ' \\n '.join(self.lines)
 
 	def __str__(self):
-		return f"lines({', '.join(self.args)}) of {len(self.lines)} lines at line {self.lineno}"
+		return f"{self.shortstr()} of {len(self.lines)} lines at line {self.lineno}"
 
 class PermuteExprsChunk:
-	def __init__(self, lineno, args, lines, direct_exprs=None, direct_original=None):
+	def __init__(self, lineno, args, optargs, lines, direct_exprs=None, direct_original=None):
 		self.lineno = lineno
 
 		if not direct_exprs:
@@ -83,25 +97,36 @@ class PermuteExprsChunk:
 			self.original = direct_original
 
 		self.args = args
+		self.optargs = optargs
 
 	def permute(self):
-		do_permute(self.args, self.exprs, self.original)
+		do_permute(self.args, self.exprs, self.optargs)
 
 	def get_text(self):
 		return ' '.join(self.exprs)
 
+	def shortstr(self):
+		return f"exprs({', '.join(self.args)})"
+
+	def dbgstate(self):
+		return ' '.join(self.exprs)
+
 	def copy(self):
-		return PermuteExprsChunk(self.lineno, self.args, None,
+		return PermuteExprsChunk(self.lineno, self.args, self.optargs, None,
 			direct_exprs=self.exprs[:], direct_original=self.original)
 
 	def __str__(self):
-		return f"exprs({', '.join(self.args)}) of {len(self.exprs)} exprs at line {self.lineno}"
+		return f"{self.shortstr()} of {len(self.exprs)} exprs at line {self.lineno}"
 
 foma_file_name = 'finnish.metafoma3'
 
 if '-h' in sys.argv:
     print(f"usage: python {sys.argv[0]} <metafoma_filename>")
     sys.exit()
+
+for i in sys.argv[1:]:
+	if not i.startswith('--'):
+		foma_file_name = i
 
 with open(foma_file_name, 'r') as fd:
 	contents = fd.read().split('\n')
@@ -121,7 +146,8 @@ for line in contents:
 		if line.strip() == "}":
 			reading_normal = True
 			constructor = PermuteExprsChunk if operator=='exprs' else PermuteLinesChunk
-			template.append(constructor(lineno - len(current), tuple(args), current))
+			fixup_optargs(current, optargs)
+			template.append(constructor(lineno - len(current), tuple(args), optargs, current))
 			current = []
 		else:
 			action, line = line.strip().split(' ', 1)
@@ -131,8 +157,17 @@ for line in contents:
 			args, line = line.split(')', 1)
 			args = [x.strip() for x in args.split(',')]
 
+			optargs = {}
+
+			if line and line[0] == '[':
+				options, line = line[1:].split(']', 1)
+				stanzas = [x.strip() for x in options.split(',')]
+				for item in stanzas:
+					k, v = item.split('=', 1)
+					optargs[k] = v
+
 			if action == 'permute':
-				assert line.strip() == "{"
+				assert line.strip() == '{'
 				reading_normal = False
 				template.append(LiteralChunk(lineno - len(current), current))
 				current = []
@@ -167,7 +202,7 @@ import random, itertools, math, multiprocessing
 param_nproc = multiprocessing.cpu_count() * 2
 param_population_size = param_nproc * 24
 param_duplicate_best = 8
-param_mutation_count = param_population_size
+param_mutation_count = param_population_size * 1.5
 
 def copytemplate(t):
 	return [x.copy() for x in t]
@@ -195,8 +230,8 @@ while True:
 
 	if '--debug' in sys.argv:
 		for item in best[1]:
-			if item[1] != 'literal':
-				print(f"   d {item[1]}({', '.join(item[2])})@{item[0]}: {' '.join(x.strip() for x in item[3])}")
+			if type(item) != LiteralChunk:
+				print(f"   d {item.shortstr()}@{item.lineno}: {item.dbgstate()}")
 
 	generation += 1
 
@@ -210,7 +245,7 @@ while True:
 			# Has nothing going for it, shuffle
 			item[1] = copytemplate(template)
 
-	for _ in range(param_mutation_count):
+	for _ in range(int(param_mutation_count)):
 		victim = random.choice(rest)[1]
 		while True:
 			item = random.choice(victim)
